f67a585f732a4ef754ccdf7f0a7a0d66045cf08b
totto <totto@totto.org>
2023-08-29 13:44:33 +0200

    missing file in last commit

--- src/main/java/no/cantara/tools/stats/domain/UserSessionStatus.java
+++ src/main/java/no/cantara/tools/stats/domain/UserSessionStatus.java
@@ -13,6 +13,8 @@ public class UserSessionStatus {
 	private int total_number_of_users;
 	private int number_of_registered_users_today;
 	private int number_of_unique_logins_today;
+	private int total_number_of_session_actions_today;
+
 	private int number_of_deleted_users_today;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 	private ZonedDateTime last_updated;
