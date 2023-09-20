345cfb32019b8ed7fadeaa9df1c4ec2be28834c6
totto <totto@totto.org>
2023-09-20 17:12:37 +0200

    minor

--- src/main/java/no/cantara/tools/stats/domain/HourlyStatus.java
+++ src/main/java/no/cantara/tools/stats/domain/HourlyStatus.java
@@ -2,5 +2,7 @@ package no.cantara.tools.stats.domain;
 
 public class HourlyStatus {
 
-    private ActivityStatistics activityStatistics = new ActivityStatistics();
+    private int number_of_registered_users_this_hour;
+    private int number_of_unique_logins_this_hour;
+    private int total_number_of_session_actions_this_hour;
 }
