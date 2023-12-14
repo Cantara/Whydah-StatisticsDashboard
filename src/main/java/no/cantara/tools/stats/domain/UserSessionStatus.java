a21df7ce8fa062825ef112fc7fc7ad7ab087e80d
totto <totto@totto.org>
2023-12-14 13:38:09 +0100

    attempting to fix a date==null issue

--- src/main/java/no/cantara/tools/stats/domain/UserSessionStatus.java
+++ src/main/java/no/cantara/tools/stats/domain/UserSessionStatus.java
@@ -20,7 +20,7 @@ public class UserSessionStatus  implements Serializable {
 
 	private int number_of_deleted_users_this_day;
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-	private ZonedDateTime last_updated;
+	private ZonedDateTime last_updated= ZonedDateTime.now();
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 	private ZonedDateTime starttime_of_this_day;
 	
